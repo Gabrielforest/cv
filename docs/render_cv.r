7245deb338da7781892342520b85e809f83cce8a
Gabrielforest <gabrielfreitaspereira@estudante.ufscar.br>
2021-05-26 19:02:19 -0300

    links updated

--- docs/render_cv.r
+++ docs/render_cv.r
@@ -18,4 +18,4 @@ rmarkdown::render(input = "docs/index.rmd",
 
 # Convert to PDF using Pagedown
 pagedown::chrome_print(input = tmp_html_cv_loc,
-                       output = "cv.pdf")
+                       output = "docs/index.pdf")
